dc0db010d5afd810364f0c8e21000539c5e82cba
Anfel <felipe0830e.e@gmail.com>
2024-03-22 20:21:00 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hola mundo")
+#solicitar al usuario que ingrese 10 números 
+numbers  = [] 
+for i in range (10):
+   num = float (input("ingresar el numero{}:".format(i+1))) 
+   numbers.append(num)
+   
+   #Calcular el promedio de la lista
+   average = sum(numbers)/len(numbers)
+   
+   #Mostrar el resultado del promedio al usuario 
+   print("El promedio de los numeros ingresados es",average)
+   
+   
+   
